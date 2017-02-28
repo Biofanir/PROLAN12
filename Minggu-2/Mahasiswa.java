4f5a9a8d8c25eff30acbb22c2ecb578e7d06fc3d
Biofanir <bio.fanir9@gmail.com>
2017-02-28 19:09:13 +0700

    Add files via upload

---
+++ Minggu-2/Mahasiswa.java
@@ -0,0 +1,9 @@
+class Mahasiswa{
+
+	public static void main(String[] args){
+		System.out.println("Nama 			: Bio Fanirwansyah Putra");
+		System.out.println("NIM 			: 10115056");
+		System.out.println("Alamat 			: Tubagus Ismail Bawah");
+		}
+		
+}
