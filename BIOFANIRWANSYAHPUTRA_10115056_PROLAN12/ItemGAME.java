7092de47ab26161b62b08599933d9e796ac73562
Biofanir <bio.fanir9@gmail.com>
2017-05-18 00:30:11 +0700

    Add files via upload

---
+++ BIOFANIRWANSYAHPUTRA_10115056_PROLAN12/ItemGAME.java
@@ -0,0 +1,37 @@
+
+public class ItemGAME {
+
+    
+   private int item1=100;
+   private int item2=130;
+   private int item3=1020;
+   private int item4=1200;
+    
+    
+    public String getItem1() {
+        System.out.println("Harga item 1 = "+item1);
+  
+        return item1;
+    }
+    
+    public String getItem2() {
+        System.out.println("Harga item 2 = "+item2);
+  
+        return item2;
+    }
+    
+    public String getItem3() {
+        System.out.println("Harga item 3 = "+item3);
+  
+        return item3;
+    }
+    
+    public String getItem4() {
+        System.out.println("Harga item 4 = "+item4);
+  
+        return item4;
+    }
+    
+   
+}
+
