310a153919a876690324270f2dc20075631bd63d
Biofanir <bio.fanir9@gmail.com>
2017-05-30 23:35:37 +0700

    Add files via upload

---
+++ minggu-12/mainbayar.java
@@ -0,0 +1,12 @@
+Public class mainbayar{
+
+    public static void main (String[] args){
+        Pembelian<Integer> beli = new Pembelian<Integer>();
+        int t= beli.sambutan();
+        System.output.println(t);
+        int y= beli.pembelianbarang();
+        System.output.println(y);
+        int z= beli.metodepembayaran();
+        System.output.println(z);
+}
+}
---
+++ minggu-12/pembelian.java
@@ -0,0 +1,48 @@
+Public class pembelian<T>{
+
+    public T pembelian_item;
+
+    public int hasil;
+    public int temp;
+
+public int pembelianbrng(){
+
+    if ( hasil > 5000){
+        hasil=hasil-1000;
+        }
+    else if(hasil == 5000){
+            hasil= hasil - 500;
+        }
+    else{
+        System.out.println("anda tidak mendapat diskon pembelian");
+    }
+    return hasil;
+}
+
+public int metodepembayaran{
+         System.out.println("1.Tunai (pembayaran anda kurang dari 5000");
+        System.out.println("2.Kredit (pembayaran anda lebih dari 5000");
+
+
+        if(hasil < 5000){
+            System.out.println("anda melakukan tunai silahkan bayar di tempat");
+            }
+        else {
+            System.out.println("anda melakukan credit");
+            hasil = hasil / 6; 
+            temp = hasil mod 6;
+            hasil = hasil + temp;
+            System.out.println("kredit anda/bulan = "+hasil);
+        }
+        return hasil;
+}    
+
+public T sambutan(){
+    System.out.println("anda memasuki pembayaran");    
+
+    return pembelian_item;
+}
+
+
+
+    
